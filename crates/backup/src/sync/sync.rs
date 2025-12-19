dc03cdd4f1b1d922094e08320f58523a266a9809
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-12-19 13:04:41 +0500

    managed the statements

--- crates/backup/src/sync/sync.rs
+++ crates/backup/src/sync/sync.rs
@@ -19,21 +19,21 @@ pub enum FileState {
 
 pub enum FileAction {
     ChangedOnly,
-    Delete,
-    DryRun,
     Verbose,
+    DryRun,
+    Delete,
 }
 
 impl SyncData {
     fn to_action(&self) -> FileAction {
         if self.changed_only {
             FileAction::ChangedOnly
-        } else if self.delete {
-            FileAction::Delete
-        } else if self.dry_run {
-            FileAction::DryRun
         } else if self.verbose {
             FileAction::Verbose
+        } else if self.dry_run {
+            FileAction::DryRun
+        } else if self.delete {
+            FileAction::Delete
         } else {
             FileAction::Verbose
         }
@@ -107,33 +107,6 @@ impl SyncData {
                     }
                 }
             }
-            FileAction::Delete => {
-                self.remove_all_dest_files();
-            }
-            FileAction::DryRun => {
-                let (_, _, state) = self.file_status();
-                match state {
-                    FileState::SrcCreated => {
-                        println!("[DRY RUN]: would copy the source file(s) to destination");
-                    }
-                    FileState::SrcModified => {
-                        println!("[DRY RUN]: would update the destination file(s)");
-                    }
-                    FileState::DestCreated => {
-                        println!(
-                            "[DRY RUN]: would prevent the creation of the destination file(s)"
-                        );
-                    }
-                    FileState::DestModified => {
-                        println!(
-                            "[DRY RUN]: would overwrite the destination file(s) with source file(s)"
-                        );
-                    }
-                    FileState::NoChange => {
-                        eprintln!("[DRY RUN]: would give the error because no changes detected");
-                    }
-                }
-            }
             FileAction::Verbose => {
                 let (modified_src_file, modified_dest_file, state) = self.file_status();
                 match state {
@@ -153,6 +126,31 @@ impl SyncData {
                         println!("[STATUS]: no changes detected");
                     }
                 }
+            }
+            FileAction::DryRun => {
+                let (_, _, state) = self.file_status();
+                match state {
+                    FileState::SrcCreated => {
+                        println!("[DRY RUN]: will copy the source file(s) to destination");
+                    }
+                    FileState::SrcModified => {
+                        println!("[DRY RUN]: will update the destination file(s)");
+                    }
+                    FileState::DestCreated => {
+                        println!("[DRY RUN]: will prevent the creation of destination file(s)");
+                    }
+                    FileState::DestModified => {
+                        println!(
+                            "[DRY RUN]: will overwrite the destination file(s) with source file(s)"
+                        );
+                    }
+                    FileState::NoChange => {
+                        eprintln!("[DRY RUN]: will give the 'no changes detected' message");
+                    }
+                }
+            }
+            FileAction::Delete => {
+                self.remove_all_dest_files();
             }
         }
     }
